888879581de8aeba6ba9401cbe0bb018c5c1a181
UnlondonGR <97224340+UnlondonGR@users.noreply.github.com>
2023-09-26 21:57:44 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+const mineflayer = require('mineflayer');
+var AutoAuth = require('mineflayer-auto-auth');
+var readline = require('readline');
+
+const bot = mineflayer.createBot({
+  host: 'ReplaceWithIP',
+  port: 25565,
+  username: 'ReplaceMe',
+  password: '',
+  version: '1.19.4',
+  plugins: [AutoAuth],
+  AutoAuth: 'ReplaceMe'
+});
+
+bot.once('spawn', () => {
+  const path = [bot.entity.position.clone()];
+  bot.on('move', () => {
+    if (path[path.length - 1].distanceTo(bot.entity.position) > 1) {
+      path.push(bot.entity.position.clone());
+    }
+  });
+});
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+rl.on('line', function(message) {
+  bot.chat(message);
+});
+
+bot.on('chat', function(username, message) {
+  if (username === bot.username) return;
+
+  const formattedMessage = formatChatMessage(username, message);
+  console.log(formattedMessage);
+});
+
+bot.on('serverAuth', function() {
+  // Here bot should be already authorized
+});
+
+// Log errors and kick reasons:
+bot.on('kicked', console.log);
+bot.on('error', console.log);
+
+// Function to format chat messages
+function formatChatMessage(username, message) {
+  return `<${username}> ${message}`;
+}
+
+const { mineflayer: mineflayerViewer } = require('prismarine-viewer')
+bot.once('spawn', () => {
+  mineflayerViewer(bot, { port: 3007, firstPerson: true }) // port is the minecraft server port, if first person is false, you get a bird's-eye view
+})
